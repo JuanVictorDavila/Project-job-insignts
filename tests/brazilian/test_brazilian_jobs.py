bc535686a97570ae45a1802b5e37d745033d6bc5
juan victor <juanvictor70@gmail.com>
2022-08-16 16:58:10 -0400

    feat:implementando os testes test_brazilian_job.py, test_counter.py, test_sorting.py

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    for job in read_brazilian_file("tests/mocks/brazilians_jobs.csv"):
+        assert "salary" in job
+        assert "title" in job
+        assert "type" in job
+    # pass
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,25 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"date_posted": "2020-05-08", "max_salary": 1000, "min_salary": 10},
+        {"date_posted": "2020-08-08", "max_salary": 10000, "min_salary": 2},
+        {"date_posted": "2020-01-08", "max_salary": 8000, "min_salary": 5},
+        {"date_posted": "2020-03-08", "max_salary": 2000, "min_salary": 7},
+        {"date_posted": "2020-09-08", "max_salary": 4000, "min_salary": 3},
+        {"date_posted": "2020-12-08", "max_salary": 6000, "min_salary": 4},
+    ]
+
+    salary_to_sort = [
+        {"date_posted": "2020-08-08", "max_salary": 10000, "min_salary": 2},
+        {"date_posted": "2020-09-08", "max_salary": 4000, "min_salary": 3},
+        {"date_posted": "2020-12-08", "max_salary": 6000, "min_salary": 4},
+        {"date_posted": "2020-01-08", "max_salary": 8000, "min_salary": 5},
+        {"date_posted": "2020-03-08", "max_salary": 2000, "min_salary": 7},
+        {"date_posted": "2020-05-08", "max_salary": 1000, "min_salary": 10},
+    ]
+    # pass
+
+    sort_by(jobs, "min_salary")
+    assert jobs == salary_to_sort
